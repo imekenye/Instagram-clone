d39ba7998dd4dee6aae66162e682282d6e952138
imekenye <ismekenye@gmail.com>
2019-05-20 09:54:26 -0700

    add instapp and accounts to the install apps in settings file

--- Instagramclone/settings.py
+++ Instagramclone/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'instapp.apps.InstappConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
