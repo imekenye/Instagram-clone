239a8f7119b35b6d8f1b40ee54679482d273db3d
imekenye <ismekenye@gmail.com>
2019-05-21 15:31:35 -0700

    initial deployment

--- accounts/views.py
+++ accounts/views.py
@@ -25,7 +25,7 @@ def login(request):
         user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
         if user is not None:
             auth.login(request, user)
-            return redirect('feed')
+            return redirect('home')
         else:
             return render(request, 'accounts/login.html', {'error': 'username or password is incorrect.'})
     else:
