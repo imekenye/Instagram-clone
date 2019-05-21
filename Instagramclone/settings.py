e0fc43fb6ec16e6c01262630e9de6639b12f2523
imekenye <ismekenye@gmail.com>
2019-05-21 15:56:00 -0700

    initial deployment

--- Instagramclone/settings.py
+++ Instagramclone/settings.py
@@ -18,7 +18,7 @@ from decouple import config, Csv
 MODE = config('MODE', default='prod')
 SECRET_KEY = config('SECRET_KEY')
 
-
+DEBUG_PROPAGATE_EXCEPTIONS = True
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
