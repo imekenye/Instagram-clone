2953f5945caa23f47b792d47ea99b2a950a3647b
imekenye <ismekenye@gmail.com>
2019-05-21 15:35:40 -0700

    initial deployment

--- Instagramclone/settings.py
+++ Instagramclone/settings.py
@@ -87,7 +87,10 @@ ROOT_URLCONF = 'Instagramclone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'accounts/templates'),
+        ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
