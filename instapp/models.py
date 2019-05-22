89896e5be9237cdaca31a3aae4fc312afa5281cb
imekenye <ismekenye@gmail.com>
2019-05-21 18:14:31 -0700

    create image model

--- instapp/models.py
+++ instapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
 
 # Create your models here.
+class Image(models.Model):
+    post_image = models.ImageField(upload_to='images/')
+    image_name = models.CharField(max_length=255)
+    image_caption = models.TextField()
+    user_profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+    comments = models.TextField()
+
+    def pub_date_format(self):
+        return self.pub_date.strftime('%b %e %Y')
