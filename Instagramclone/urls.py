675bf5da3a51530f31630e2b58e64e7439f19eb1
imekenye <ismekenye@gmail.com>
2019-05-22 08:49:24 -0700

    update urls file

--- Instagramclone/urls.py
+++ Instagramclone/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', views.home, name='home'),
-                  path('accounts/', include('accounts.urls')),
-                  path('instapp/', include('instapp.urls')),
-              ]
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('accounts/', include('accounts.urls')),
+    path('instapp/', include('instapp.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
