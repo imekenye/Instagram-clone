beeceb5c2246c98df35a4474e405dcdf79cb0dd0
imekenye <ismekenye@gmail.com>
2019-05-22 08:49:44 -0700

    update urls file

--- instapp/urls.py
+++ instapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from .import views
 
 urlpatterns = [
-    path('post', views.post, name='post')
+    path('post', views.post, name='post'),
+    path('<int:image_id>', views.imagedetail, name='imagedetail'),
+    path('<int:image_id/like>', views.like, name='like'),
 ]
